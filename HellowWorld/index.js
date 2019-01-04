8f4ce977d59cb6e14753751a80e9112a2117271b
Reda <reda.bakr@gmail.com>
2019-01-04 18:55:25 +0300

    Added info

--- HellowWorld/index.js
+++ HellowWorld/index.js
@@ -1,6 +1,8 @@
+var info = require('../Shared/index');
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
+    context.log(info.name);
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
---
+++ Shared/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    info:{
+        name : 'reda bakr'
+    }
+}
